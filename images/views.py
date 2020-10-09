5fc1b07c8775aac10b22aa47ca4916f989d36a27
Kate <you@example.com>
2020-10-09 18:57:33 +0300

    error handing

--- images/views.py
+++ images/views.py
@@ -44,3 +44,13 @@ def past_days_images(request,past_date):
         </html>
             '''
     return HttpResponse(html)
+
+def past_days_images(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
