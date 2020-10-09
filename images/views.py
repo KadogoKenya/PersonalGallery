114b945b921e71606e8884c92263910ecbf976c6
Kate <you@example.com>
2020-10-09 20:05:22 +0300

    linked templates with views

--- images/views.py
+++ images/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
 from django.http  import HttpResponse,Http404
@@ -26,25 +26,17 @@ def convert_dates(dates):
     return day
 
 def past_days_images(request,past_date):
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Images for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-
-def past_days_images(request,past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-images/past-images.html', {"date": date})
