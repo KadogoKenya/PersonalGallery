ffec705c307346924648dfbd484b43f2a2153d7c
Kate <you@example.com>
2020-10-09 15:54:38 +0300

    urlconf add

--- tribune/urls.py
+++ tribune/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url,include
+from django.contrib import admin
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^images/',include('news.urls')),
 ]
