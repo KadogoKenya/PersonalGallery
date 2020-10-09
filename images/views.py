48ee1f57e04163ee6e7962e0cf3b8c67654eb5be
Kate <you@example.com>
2020-10-09 19:28:58 +0300

    created our base template

--- images/views.py
+++ images/views.py
@@ -5,7 +5,7 @@ from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse(request,'welcome.html')
+    return render HttpResponse(request,'welcome.html')
 
 def images_of_day(request):
     date = dt.date.today()
