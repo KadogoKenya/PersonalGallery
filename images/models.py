a8b4ee2a46a77733c6d866a89f0e8a413623ef0e
Kate <you@example.com>
2020-10-10 07:00:13 +0300

    models created

--- images/models.py
+++ images/models.py
@@ -26,7 +26,7 @@ class location(models.Model):
         return self.name
 
 class category(models.Model):
-    name = models.CharField(max_length =30)
+    name = models.CharField(max_length =255)
 
     def __str__(self):
         return self.name
