c9d359d18957c01474f5c05ada285292c705fb24
Kate <you@example.com>
2020-10-10 06:30:21 +0300

    creating models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'location/')
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =1000)
+    
+   
