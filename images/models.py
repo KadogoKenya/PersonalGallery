fa7310a80730f71ba005c9dc1f850b8a1d3b37ee
Kate <you@example.com>
2020-10-10 11:22:14 +0300

    tests added

--- images/models.py
+++ images/models.py
@@ -8,7 +8,7 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length =100)
     
 
@@ -17,13 +17,12 @@ class category(models.Model):
    
 class Image(models.Model):
     image = models.ImageField(upload_to = 'location/')
-    image_name = models.CharField(max_length =100)
-    image_description = models.TextField()
+    name = models.CharField(max_length =100)
+    description = models.TextField()
 
     author = models.CharField(max_length=30, default='admin')
 
-    date = models.DateTimeField(auto_now_add=True)
-
+    pub_date = models.DateTimeField(auto_now_add=True)
     location = models.ForeignKey(Location,on_delete=models.SET_NULL, null=True)
 
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-from .models import Image,Location,category
+from .models import Image,Location,Category
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.imagetest= Image(id=1,name = 'image', description ='try this sample test image', location=self.location,category=self.category)
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.imagetest,Image))
