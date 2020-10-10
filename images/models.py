c857b7de8efb72e7a3af927a3807a39718e84170
Kate <you@example.com>
2020-10-10 10:40:12 +0300

    models migrated

--- images/models.py
+++ images/models.py
@@ -2,41 +2,26 @@ from django.db import models
 from django.db import models
 
 
-# Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.first_name
-
-    class Meta:
-        ordering = ['first_name']
-
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'location/')
-    image_name = models.CharField(max_length =30)
-    image_description = models.CharField(max_length =1000)
-    editor = models.ForeignKey(Editor)
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
 class Location(models.Model):
-    name = models.CharField(max_length =30)
+    name = models.CharField(max_length =100)
 
     def __str__(self):
         return self.name
 
 class Category(models.Model):
-    name = models.CharField(max_length =30)
-    <label for="category">Choose a category for images:</label>
-    <select name="category" id="category">
-        <option value="Travel">Travel</option>
-        <option value="Food & drinks">Food & drinks</option>
-        <option value="Birthday">Birthday</option>
-        <option value="Wedding">Wedding</option>
-    </select>
+    name = models.CharField(max_length =100)
+    
 
     def __str__(self):
         return self.name
    
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length =100)
+    image_description = models.TextField()
+
+    editor = models.CharField(max_length=30, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+
+    location = models.ForeignKey(Location,on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
