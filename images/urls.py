842aa43761030dd73a8e942f2c82f1be58b31121
Kate <you@example.com>
2020-10-11 10:42:06 +0300

    image upload processor

--- images/urls.py
+++ images/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns=[
     url('^$',views.images_of_day,name='imagesToday'),
     # url('^today/$',views.images_of_day,name='imagesToday'),
