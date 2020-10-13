128fae63e2b32f2947016d3f782ca7f56c4517ed
Kate <you@example.com>
2020-10-13 11:05:58 +0300

    final changes

--- images/views.py
+++ images/views.py
@@ -59,3 +59,6 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-images/search.html',{"message":message})
+
+
+        
